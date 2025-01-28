fd19b0cc5bb2b400a080966eb4ae1389493decfa
manishmanume <149936975+manishmanume@users.noreply.github.com>
2025-01-28 21:51:15 +0530

    change json

--- Index.js
+++ Index.js
@@ -7,48 +7,55 @@ const path = require('path');
 
 dotenv.config({ path: "./.env" });
 
-const corsOrigins = ["http://localhost:5173", "http://localhost:3000", "https://onlinesshoping.netlify.app/"];
+const corsOrigins = [
+  "https://onlineshopiing.netlify.app",
+   "http://localhost:5173",
+  "https://6797979f711d8600086e1e37--onlineshopiing.netlify.app",
+];
+
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || corsOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            console.error(`Blocked by CORS: ${origin}`);
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
+  origin: (origin, callback) => {
+      if (!origin || corsOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          console.error(`Blocked by CORS: ${origin}`);
+          callback(new Error("Not allowed by CORS"));
+      }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
+
 
 const app = express();
 
-// Middleware
+app.use(cors(corsOptions));
+
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Static File Serving
+app.options('*', cors(corsOptions));
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Routes
 app.use('/api', Router);
 
 app.set('port', process.env.PORT || 3000);
+app.options('*', cors(corsOptions));
 
-// Test Route
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
-// Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send({ message: 'An error occurred!', error: err.message });
 });
 
-// Start Server
 app.listen(app.get('port'), () => {
     console.info(`Server listening on port ${app.get('port')}`);
 });
