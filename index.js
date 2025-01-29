58dddfccdbbc08447c7d8f3aebe3ef42ecd79923
Manish Mehta <149936975+manishmanume@users.noreply.github.com>
2025-01-29 19:15:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ const corsOptions = {
 const app = express();
 
 app.use(cors(corsOptions));
-
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
